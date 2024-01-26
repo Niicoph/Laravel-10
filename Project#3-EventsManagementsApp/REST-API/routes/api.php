83b58ee6ed356e71d6ba7e7051a05e547c53ee53
Nicolas Pesce <niicoph@gmail.com>
2024-01-26 15:35:02 -0300

    starting project #3

--- Project#3-EventsManagementsApp/REST-API/routes/api.php
+++ Project#3-EventsManagementsApp/REST-API/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('events' , EventController::class);
+        // Attendees do not exist without an event, they always need to be part of an event
+Route::apiResource('events.attendees' , AttendeeController::class)
+        ->scoped(['attendee' => 'event']);
+        /*
+        -This will allow to get the attendees of a specific event
+        -the attende resource is always part of an event
+        if we check the routes we will see 
+            -> api/events/{event}/attendees/{attendee}
+        This means that the attendee resource is always part of an event (parent)
+        and if a parent(which its the event) is not defined or it doesnt exist, this will cause an error
+        */
+
