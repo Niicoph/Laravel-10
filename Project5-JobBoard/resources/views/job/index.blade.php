9d10346782f3b6b66b62c2c2ca697a413f6d9553
Nicolas Pesce <niicoph@gmail.com>
2024-02-20 11:36:44 -0300

    keeping up with project 5

--- Project5-JobBoard/resources/views/job/index.blade.php
+++ Project5-JobBoard/resources/views/job/index.blade.php
@@ -1,5 +1,36 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]"></x-breadcrumbs>
+   
+    <x-card class="mb-4 text-sm">
+        <form id="filtering-form" action="{{route('jobs.index')}}" method="GET">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+          <div>
+            <div class="mb-1 font-semibold">Search</div>
+            <x-text-input name="search" value="" placeholder="Search for any text" form-id="filtering-form" />
+          </div>
+          <div>
+            <div class="mb-1 font-semibold">Salary</div>
+    
+            <div class="flex space-x-2">
+              <x-text-input name="min_salary" value="{{request('min_salary')}}" placeholder="From"  form-id="filtering-form" />
+              <x-text-input name="max_salary" value="{{request('max_salary')}}" placeholder="To"  form-id="filtering-form" />
+            </div>
+          </div>
+          <div>
+            <div class="mb-1 font-semibold">Experience</div>
+              <x-radio-group name="experience" :options="array_combine(array_map('ucfirst', \App\Models\Job::$experience) , \App\Models\Job::$experience)"  ></x-radio-group>
+          </div>
+          <div>
+            <div class="mb-1 font-semibold">Category</div>
+            <x-radio-group name="category" :options="\App\Models\Job::$category"  ></x-radio-group>
+          </div>
+        </div>
+
+        <button class="btn w-full border p-2 rounded-md font-bold ">Filter</button>
+
+       </form>
+    </x-card>
+
     @foreach ($jobs as $job) 
         <x-job-card class="mb-4" :job="$job">
          
